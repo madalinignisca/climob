1fa8dca9f9e256a6f4fd6f4dd967bfd48cb6433f
Madalin Ignisca <git@madalin.me>
2023-11-11 09:08:35 +0000

    Prepare admin with filament.

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,13 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -42,4 +43,12 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+	/**
+	 *
+	 * @param \Filament\Panel $panel
+	 * @return bool
+	 */
+	public function canAccessPanel(\Filament\Panel $panel) {
+        return $this->is_admin;
+	}
 }
